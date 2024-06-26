f97efccb53de9a5264d92fa03abd6b239c4c0b50
heliosalesjr <heliosalesjr@gmail.com>
2024-06-26 20:23:47 -0300

    data fetching with typescript

--- comh/app/layout.tsx
+++ comh/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
+import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +26,10 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <main className="max-w-3xl mx-auto px-4 py-5">
+            <Navbar />
+             {children}
+            </main>
           </ThemeProvider>
       </body>
     </html>
--- comh/app/page.tsx
+++ comh/app/page.tsx
@@ -1,11 +1,34 @@
+import { Card } from "@/components/ui/card";
+import { simpleBlogCard } from "./lib/interface";
+import { client, urlFor } from "./lib/sanity";
 import Image from "next/image";
-import Navbar from "./components/Navbar";
 
-export default function Home() {
+
+async function getData() {
+  const query = `
+    *[_type == 'blog'] | order(_createdAt desc){
+      title,
+        smallDescription,
+        "currentSlug" : slug.current,
+        titleImage
+    }`;
+
+    const data = await client.fetch(query);
+    return data
+}
+
+export default async function Home() {
+
+  const data: simpleBlogCard[] = await getData();
+  console.log(data);
   return (
-    <>
-      <Navbar />
-      <p>Hey I am the index</p>
-    </>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5 gap-5">
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image src={urlFor(post.titleImage).url()} alt={post.smallDescription} width={500} height={500}/>
+        </Card>
+      ))}
+      
+    </div>
   );
 }
