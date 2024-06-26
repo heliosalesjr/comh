0c177dc97beec6b29f5b918b0b94b725085023f6
heliosalesjr <heliosalesjr@gmail.com>
2024-06-26 15:08:23 -0300

    dark mode on!!!

--- comh/app/components/Navbar.tsx
+++ comh/app/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
 
 
 export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-3xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
-        Hélio's<span className="text-primary">Blog</span>
+        Hélio<span className="text-primary font-black">Blog</span>
       </Link>
-
+    
+        <ModeToggle/>
       
     </nav>
   );
