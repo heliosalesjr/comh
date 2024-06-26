936105fc1dbbb06c7b1b9b217e079f95a9d575b7
heliosalesjr <heliosalesjr@gmail.com>
2024-06-26 20:28:15 -0300

    post cards data fetching + styling

--- comh/app/page.tsx
+++ comh/app/page.tsx
@@ -1,7 +1,9 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { simpleBlogCard } from "./lib/interface";
 import { client, urlFor } from "./lib/sanity";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 async function getData() {
@@ -26,6 +28,16 @@ export default async function Home() {
       {data.map((post, idx) => (
         <Card key={idx}>
           <Image src={urlFor(post.titleImage).url()} alt={post.smallDescription} width={500} height={500}/>
+
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+              {post.smallDescription}
+            </p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+          </CardContent>
         </Card>
       ))}
       
