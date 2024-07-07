c85386911e291d4592bff7832a72ad41340bcb7c
heliosalesjr <heliosalesjr@gmail.com>
2024-07-06 23:23:48 -0300

    nav tailwind styling

--- comh/app/components/Navbar.tsx
+++ comh/app/components/Navbar.tsx
@@ -4,13 +4,16 @@ import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="w-full relative flex items-center justify-between max-w-5xl mx-auto px-4 py-5">
+    <nav className="w-full relative flex items-center justify-between md:justify-between max-w-5xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
         Hélio<span className="text-primary font-black">Blog</span>
       </Link>
-      <p className="text-sm px-4 max-w-xl mx-auto">A little description here... Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis inventore beatae nam perspiciatis quasi omnis quisquam deleniti doloremque dicta consectetur tempore, libero alias, voluptatem expedita, ratione in reprehenderit sequi debitis?</p>
-      <ModeToggle/>
-      
+      <p className="hidden md:block px-8 text-sm">
+        A little description here... Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis inventore beatae nam perspiciatis quasi omnis quisquam deleniti doloremque dicta consectetur tempore.
+      </p>
+      <div className="ml-auto md:ml-0">
+        <ModeToggle />
+      </div>
     </nav>
   );
 }
